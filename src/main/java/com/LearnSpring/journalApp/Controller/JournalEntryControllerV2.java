873f631f2913cb9378142e33239467e7ffd53bb7
imadityak <adityacoding.one@gmail.com>
2024-10-29 17:07:05 +0530

    Configure lombok

--- src/main/java/com/LearnSpring/journalApp/Controller/JournalEntryControllerV2.java
+++ src/main/java/com/LearnSpring/journalApp/Controller/JournalEntryControllerV2.java
@@ -4,9 +4,12 @@ import com.LearnSpring.journalApp.entity.JournalEntry;
 import com.LearnSpring.journalApp.services.JournalEntryServices;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 // the end point path will be /journal/abc
 //RequestMapping will map your whole class
@@ -18,36 +21,51 @@ public class JournalEntryControllerV2 {
     private JournalEntryServices journalEntryServices;
 
     @GetMapping
-    public List<JournalEntry> getAll() {
-        return journalEntryServices.getAll();
+    public ResponseEntity<?> getAll() {
+        List<JournalEntry> all = journalEntryServices.getAll();
+        if(all != null && !all.isEmpty()){
+            return new ResponseEntity<>(all, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @PostMapping
-    public JournalEntry createEntry(@RequestBody JournalEntry myEntry) {
-        myEntry.setDate(LocalDateTime.now());
-        journalEntryServices.saveEntry(myEntry);
-        return myEntry;
+    public ResponseEntity<JournalEntry> createEntry(@RequestBody JournalEntry myEntry) {
+        try{
+            myEntry.setDate(LocalDateTime.now());
+            journalEntryServices.saveEntry(myEntry);
+            return new ResponseEntity<>(myEntry, HttpStatus.CREATED);
+        }
+        catch(Exception e){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
 
+    // here we are using the response entity so that we can get correct https status from server
     @GetMapping("id/{myId}")
-    public JournalEntry getJournalEntryById(@PathVariable ObjectId myId) {
-        return journalEntryServices.entryById(myId).orElse(null);
+    public ResponseEntity<JournalEntry> getJournalEntryById(@PathVariable ObjectId myId) {
+        Optional<JournalEntry> journalEntry = journalEntryServices.entryById(myId);
+        if (journalEntry.isPresent()){
+            return new ResponseEntity<>(journalEntry.get(), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @PutMapping("id/{myId}")
-    public JournalEntry updateJournalEntryById(@PathVariable ObjectId myId, @RequestBody JournalEntry newEntry) {
+    public ResponseEntity<JournalEntry> updateJournalEntryById(@PathVariable ObjectId myId, @RequestBody JournalEntry newEntry) {
         JournalEntry old = journalEntryServices.entryById(myId).orElse(null);
         if(old != null){
             old.setTitle(newEntry.getTitle() != null && !newEntry.getTitle().equals("") ? newEntry.getTitle() : old.getTitle());
             old.setContent(newEntry.getContent() != null && !newEntry.getContent().equals("") ? newEntry.getContent() : old.getContent());
+            journalEntryServices.saveEntry(old);
+            return new ResponseEntity<>(old,HttpStatus.OK);
         }
-        journalEntryServices.saveEntry(old);
-        return old;
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @DeleteMapping("id/{myId}")
-    public boolean deleteJournalEntryById(@PathVariable ObjectId  myId) {
+    public ResponseEntity<?> deleteJournalEntryById(@PathVariable ObjectId  myId) {
         journalEntryServices.deleteById(myId);
-        return true;
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
--- src/main/java/com/LearnSpring/journalApp/entity/JournalEntry.java
+++ src/main/java/com/LearnSpring/journalApp/entity/JournalEntry.java
@@ -1,5 +1,6 @@
 package com.LearnSpring.journalApp.entity;
 
+import lombok.Data;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -10,45 +11,14 @@ import java.util.Date;
 // plain old java object(POJO) class - This is the basic structure of the journal entries
 // here we are converting this class to relate with database
 // now every journal will act as a document in mongoDb
+// Lombok -> 1 annotation(@Data) can clearly add all these plugins -> Setter,Getter,RequiredConstructor
 
 @Document(collection = "journal_entries")
+@Data
 public class JournalEntry {
     @Id
     private ObjectId id;
     private String title;
     private String content;
-
     private LocalDateTime date;
-
-    public LocalDateTime getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
-
-    public ObjectId getId() {
-        return id;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
 }
